bf1b206c922f55aed2870186cf569639f407757b
thomasmullen9999 <thomasmullen9999@gmail.com>
2024-02-22 16:21:32 +0000

    Destructured app in listen.js to fix bug

--- listen.js
+++ listen.js
@@ -1,4 +1,6 @@
-const app = require('./app.js');
+const { app } = require('./app.js');
 const { PORT = 9090 } = process.env;
+
+console.log(app)
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
