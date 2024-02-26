169ec4a3d105ee44f1a1c93eb2574e327d8af8ca
thomasmullen9999 <thomasmullen9999@gmail.com>
2024-02-26 13:24:29 +0000

    added DELETE /api/articles/:article_id endpoint

--- model/model.js
+++ model/model.js
@@ -292,3 +292,16 @@ exports.insertNewTopic = (newTopic) => {
       return topic.rows[0]
     })
 }
+
+exports.removeArticleById = (articleId) => {
+  return db.query(
+    `DELETE FROM articles
+    WHERE article_id = $1 
+    RETURNING *;`, 
+    [articleId])
+    .then((article) => {
+      if (article.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found" })
+      }
+    })
+}
